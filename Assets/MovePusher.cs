09ea0cf30de45373a2985914c952fad576b01291
mikasa <mikasanow@gmail.com>
2015-04-16 18:53:19 +0900

    update

--- Assets/MovePusher.cs
+++ Assets/MovePusher.cs
@@ -8,12 +8,30 @@ public class MovePusher : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		Debug.Log ("Let's CoinPusher !!");
+
+		// Pusher初期位置取得
 		origin = GetComponent<Rigidbody> ().position;
 	}
 	
 	// Update is called once per frame
+	float speed = 0.02f;
 	void Update () {
-		Vector3 offset = new Vector3 (0, 0, Mathf.Sin (Time.time));
-		GetComponent<Rigidbody> ().MovePosition (origin + offset);
+		//sin使う場合
+		//Vector3 offset = new Vector3 (0, 0, Mathf.Sin (Time.time));
+		//GetComponent<Rigidbody> ().MovePosition (origin + offset);
+
+		Vector3 pos = GetComponent<Rigidbody> ().position;
+		Debug.Log (pos.z);
+		if (pos.z >= origin.z + 1.0f) {
+			speed = -0.02f;
+		} else if (pos.z <= origin.z - 1.0f) {
+			speed = 0.02f;
+		}
+		GetComponent<Rigidbody> ().MovePosition (new Vector3(pos.x, pos.y, pos.z + speed));
+
+
+
 	}
 }
